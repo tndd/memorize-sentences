7b044ed9aee004bfe95bd298083fecb5c8d81120
tau <ttru702@gmail.com>
2023-01-12 17:44:54 +0900

    restruct

--- stats.py
+++ stats.py
@@ -4,12 +4,11 @@ from enum import Enum, auto
 from broker import get_df_records, get_df_sentences
 
 
-def get_sr_mean_of_selected_by_sentence(df, unknown_id_value):
+def get_sr_wrong_rate_by_sentence(df, unknown_id_value):
     sr_mean = df.groupby('sentence_id').mean(numeric_only=True)['status']
     full_sentence_ids = get_df_sentences().index
-    # Unanswered sentence_id shall be assumed to have been answered wrong.
-    sr_mean = sr_mean.reindex(full_sentence_ids, fill_value=unknown_id_value)
-    sr_mean_selected = sr_mean.apply(lambda x: max(1 - x, 0.01))
+    sr_mean_filled_un = sr_mean.reindex(full_sentence_ids, fill_value=unknown_id_value)
+    sr_mean_selected = sr_mean_filled_un.apply(lambda x: max(1 - x, 0.01))
     return sr_mean_selected.sort_values(ascending=False)
 
 
@@ -18,7 +17,7 @@ class GetSentencesMode(Enum):
     REVIEW = auto()
 
 
-def get_random_sentence_ids(mode=GetSentencesMode.FULL, n=None):
+def get_sr_probs_of_selected_ids(mode):
     if mode == GetSentencesMode.FULL:
         record_since = 'all'
         unknown_id_value = 0 # Unanswered sentence_id are treated as "WRONG".
@@ -26,18 +25,21 @@ def get_random_sentence_ids(mode=GetSentencesMode.FULL, n=None):
         record_since = 'latest'
         unknown_id_value = 1 # Unanswered sentence_id are treated as "CORRECT".
     df = get_df_records(since=record_since)
-    sr = get_sr_mean_of_selected_by_sentence(df, unknown_id_value)
-    prob = sr / sr.sum()
-    print(prob)
-    n = len(sr) if n is None else n
-    return random.choices(sr.index,weights=prob, k=n)
+    sr = get_sr_wrong_rate_by_sentence(df, unknown_id_value)
+    return sr / sr.sum()
+
+
+def get_random_sentence_ids(mode=GetSentencesMode.FULL, n=None):
+    prob_selected = get_sr_probs_of_selected_ids(mode)
+    n = len(prob_selected) if n is None else n
+    return random.choices(prob_selected.index,weights=prob_selected, k=n)
 
 
 def main() -> None:
     ids = get_random_sentence_ids(mode=GetSentencesMode.FULL)
     print(ids)
-    ids = get_random_sentence_ids(mode=GetSentencesMode.REVIEW)
-    print(ids)
+    # ids = get_random_sentence_ids(mode=GetSentencesMode.REVIEW)
+    # print(ids)
 
 
 if __name__ == '__main__':
