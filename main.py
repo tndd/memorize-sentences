b102eba804610c0fc17dfd8a40275ed4005c2787
tau <ttru702@gmail.com>
2023-01-12 18:31:21 +0900

    add comments

--- main.py
+++ main.py
@@ -54,6 +54,10 @@ def test_sentences(sentences, n=None):
                     'diff': diff
                 }
                 break
+    return paper
+
+
+def store_test_paper(paper):
     file_name = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     dir_name = f"record/{file_name.split('T')[0]}"
     os.makedirs(dir_name, exist_ok=True)
@@ -63,21 +67,31 @@ def test_sentences(sentences, n=None):
 
 def main() -> None:
     # Params
+    # TODO support for optional args.
     mode = TestMode.FULL
     n = 2
 
+    # get all data of senteneces for test
     df_sentences = get_df_sentences()
+    # select mode of test
     if mode == TestMode.FULL:
         record_since = 'all'
         unknown_id_value = 0 # Unanswered sentence_id are treated as "WRONG".
     if mode == TestMode.REVIEW:
         record_since = 'latest'
         unknown_id_value = 1 # Unanswered sentence_id are treated as "CORRECT".
+    # get data of records for narrow down to focus on failure sentence
     df_records = get_df_records(since=record_since)
+    # get series of wrong rate by sentences
     wrong_rate_by_sentences = get_sr_wrong_rate_by_sentence(df_records, df_sentences.index, unknown_id_value)
-    ids_choiced = get_random_sentence_ids(wrong_rate_by_sentences, n)
-    df_sentences_for_test = df_sentences.loc[ids_choiced]
-    test_sentences(df_sentences_for_test)
+    # randomly select in proportion to wrong rate
+    ids_selected = get_random_sentence_ids(wrong_rate_by_sentences, n)
+    # narrow down the test sentences to only select id
+    df_sentences_for_test = df_sentences.loc[ids_selected]
+    # do test
+    test_paper = test_sentences(df_sentences_for_test)
+    # store test results
+    store_test_paper(test_paper)
 
 
 if __name__ == '__main__':
