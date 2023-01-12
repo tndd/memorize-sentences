bf7e895e20861105b496cae4a97e467a63cf33c2
tau <ttru702@gmail.com>
2023-01-12 19:17:48 +0900

    fixed to save the percentage of correct answers

--- broker.py
+++ broker.py
@@ -1,8 +1,12 @@
 import json
+import os
 import re
 from glob import glob
 
 import pandas as pd
+
+BASE_DIR_NAME_TEST_PAPER = 'record/paper'
+BASE_DIR_NAME_TEST_STATS = 'record/stats'
 
 
 def _build_df_sentence(file_path):
@@ -27,13 +31,13 @@ def _build_df_record(file_path):
     with open(file_path, 'r') as f:
         d = json.load(f)
     df = pd.DataFrame.from_dict(d, orient='index')
-    group_name = re.search(r"record/.*\/(.*)\.json", file_path).group(1)
+    group_name = re.search(f"{BASE_DIR_NAME_TEST_PAPER}/.*/(.*)\.json", file_path).group(1)
     df.insert(0, 'group', group_name)
     return df
 
 
 def get_df_records(since='latest'):
-    record_file_paths = glob('record/*/*.json')
+    record_file_paths = glob(f'{BASE_DIR_NAME_TEST_PAPER}/*/*.json')
     df = pd.concat([_build_df_record(fp) for fp in record_file_paths])
     df.index.name = 'sentence_id'
     df = df.reset_index()
@@ -44,3 +48,24 @@ def get_df_records(since='latest'):
         return df[df['group'] == latest_date]
     else:
         return df[df['group'] >= since]
+
+
+def store_test_paper(file_name, paper):
+    dir_name = f"{BASE_DIR_NAME_TEST_PAPER}/{file_name.split('T')[0]}"
+    os.makedirs(dir_name, exist_ok=True)
+    with open(f"{dir_name}/{file_name}.json", 'w') as f:
+        json.dump(paper, f, indent=4)
+
+
+def store_test_stat(file_name, df_stats):
+    os.makedirs(BASE_DIR_NAME_TEST_STATS, exist_ok=True)
+    df_stats.to_csv(f"{BASE_DIR_NAME_TEST_STATS}/{file_name}.tsv", sep='\t')
+
+
+def main() -> None:
+    df = get_df_records('all')
+    print(df)
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import difflib
-import json
-import os
 from datetime import datetime
 from enum import Enum, auto
 
-from broker import get_df_records, get_df_sentences
+import pandas as pd
+
+from broker import (get_df_records, get_df_sentences, store_test_paper,
+                    store_test_stat)
 from stats import get_random_sentence_ids, get_sr_wrong_rate_by_sentence
 
 
@@ -56,22 +57,7 @@ def test_sentences(sentences, n=None):
     return paper
 
 
-def store_test_paper(paper):
-    file_name = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-    dir_name = f"record/{file_name.split('T')[0]}"
-    os.makedirs(dir_name, exist_ok=True)
-    with open(f"{dir_name}/{file_name}.json", 'w') as f:
-        json.dump(paper, f, indent=4)
-
-
-def main() -> None:
-    # Params
-    # TODO support for optional args.
-    mode = TestMode.FULL
-    n = 2
-
-    # get all data of senteneces for test
-    df_sentences = get_df_sentences()
+def get_wrong_rate_by_sentences(mode, df_sentences):
     # select mode of test
     if mode == TestMode.FULL:
         record_since = 'all'
@@ -82,15 +68,38 @@ def main() -> None:
     # get data of records for narrow down to focus on failure sentence
     df_records = get_df_records(since=record_since)
     # get series of wrong rate by sentences
-    wrong_rate_by_sentences = get_sr_wrong_rate_by_sentence(df_records, df_sentences.index, unknown_id_value)
+    return get_sr_wrong_rate_by_sentence(df_records, df_sentences.index, unknown_id_value)
+
+
+def get_df_test_stats(df_sentences):
+    mode = TestMode.FULL
+    wrong_rate_by_sentences = get_wrong_rate_by_sentences(mode, df_sentences)
+    return pd.merge(wrong_rate_by_sentences, df_sentences, left_index=True, right_index=True)
+
+
+def main() -> None:
+    # Params
+    # TODO support for optional args.
+    mode = TestMode.FULL
+    n = 1
+
+    # get all data of senteneces for test
+    df_sentences = get_df_sentences()
+    # get series of wrong rate by sentence id
+    wrong_rate_by_sentences = get_wrong_rate_by_sentences(mode, df_sentences)
     # randomly select in proportion to wrong rate
     ids_selected = get_random_sentence_ids(wrong_rate_by_sentences, n)
     # narrow down the test sentences to only select id
     df_sentences_for_test = df_sentences.loc[ids_selected]
     # do test
     test_paper = test_sentences(df_sentences_for_test)
-    # store test results
-    store_test_paper(test_paper)
+    # # store test results
+    file_name = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+    store_test_paper(file_name, test_paper)
+    # store test stat
+    df_test_stat_updated = get_df_test_stats(df_sentences)
+    store_test_stat(file_name, df_test_stat_updated)
+
 
 
 if __name__ == '__main__':
